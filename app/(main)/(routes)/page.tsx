0041f7c8ef02675e68fc418991667a3e2f152324
Roman <roman.shemelin31@gmail.com>
2023-08-26 15:16:50 +0300

    theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-end justify-end gap-5 pt-5">
+      <ModeToggle/>
       <UserButton afterSignOutUrl="/"/>
     </div>
   )
