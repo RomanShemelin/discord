ddcae251ab230420504e2586c5a01723f60229ea
Roman <roman.shemelin31@gmail.com>
2023-08-28 22:40:13 +0300

    add file uploads dropzone

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -21,6 +21,7 @@ import {
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { useEffect, useState } from 'react';
+import FileUpload from '../file-upload';
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -52,7 +53,7 @@ const InitialModal = () => {
   if (!isMounted) {
     return null;
   }
-  
+
   return (
     <Dialog open>
       <DialogContent className='bg-white text-black p-0 overflow-hidden'>
@@ -65,7 +66,21 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <div className='space-y-6 m-6'>
               <div className='flex items-center justify-center text-center'>
-                Image upload
+                <FormField
+                  control={form.control}
+                  name='imageUrl'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint='serverImage'
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
