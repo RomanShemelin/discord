c44016da3aa7a543c0bfebe93f5838b1782e7922
Roman <roman.shemelin31@gmail.com>
2023-09-02 16:48:19 +0300

    create sidebar

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,21 @@
+import { Plus } from 'lucide-react';
+import { ActionTooltip } from '@/components/ui/action-tooltip';
+
+const NavigationAction = () => {
+  return (
+    <div>
+      <ActionTooltip label='Add a server' align='center' side='right'>
+        <button className='group flex items-center mt-5'>
+          <div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'>
+            <Plus
+              className='group-hover:text-white transition text-emerald-500'
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  );
+};
+
+export default NavigationAction;
