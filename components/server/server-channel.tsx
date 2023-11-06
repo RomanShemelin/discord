89608e9ba3c817a94ecbfbbf10a4c5c5f0d865f6
Roman <roman.shemelin31@gmail.com>
2023-11-06 15:23:25 +0300

    fix server channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -26,7 +26,7 @@ export const ServerChannel = ({ channel, server, role }: ServerChannel) => {
   const Icon = iconMap[channel.type];
 
   const onClick = () => {
-    router.push(`/servers/${params.serverId}/channels/${channel.id}`);
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
   };
 
   const onAction = (e: React.MouseEvent, action: ModalType) => {
